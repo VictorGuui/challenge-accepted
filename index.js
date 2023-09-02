88ed4f6e0e4e16be1c51f166a1ac6b8271eb4a95
VictorGuui <victorguilhermebranco@outlook.com>
2023-09-02 00:27:22 -0300

    Docker

--- index.js
+++ index.js
@@ -1,33 +1,51 @@
+/*
+    Arquivo: ./index.js
+    Função: Iniciar o app web
+    npm run start
+*/
+
 const porta = 8080;
 const express = require("express");
 
 const locales = require("./base/locales.json");
-const weather = require("./base/weather.json");
+const raw_weather = require("./base/weather.json");
 
-const servidor = express();
-const app = servidor;
+const weather = [];
 
-servidor.use(express.static(__dirname + "/"));
+for (let value of raw_weather) {
+  key = value.locale.id;
+  weather[key] = value;
+}
 
-servidor.engine("html", require("ejs").renderFile);
-servidor.set("view engine", "html");
+const app = express();
 
-servidor.get("/", function (req, res) {
-  res.render("index"); // carrega ./views/index.html
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
-servidor.get("/about", function (req, res) {
-  res.render("about"); // carrega ./views/about.html
-});
+app.use(express.static(__dirname + "/"));
 
+app.get("/", (req, res) => {res.send("still awake")})
 app.get("/locales", (req, res) => {
   res.send(locales);
+});
+
+app.get("/weather/:id", (req, res) => {
+  data_weather = weather[req.params.id];
+  if (data_weather != undefined) {
+    res.send(data_weather);
+  } else {
+    res.status(404).send("sorry");
+  }
 });
 
 app.get("/locales/:id", (req, res) => {
   res.send(locales[req.params.id]);
 });
 
-servidor.listen(porta, () => {
-  console.log(`Server is running on port ${porta}`);
-});
+app.listen(porta);
